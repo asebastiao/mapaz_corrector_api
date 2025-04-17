39649713753e46a250ce2aebc7bace252b301e28
Antonio Sebastiao <asebasti@c3r5s2.42luanda.com>
2025-04-17 10:20:35 +0100

    Implementing a simple home template template

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI, Depends, Request
+from fastapi import FastAPI, Request, Depends
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from auth import router as auth_router
 from spell_check import corrigir_frase
 from config import Settings
@@ -7,20 +9,23 @@ from database import init_db
 
 app = FastAPI()
 
+templates = Jinja2Templates(directory="templates")
+
 init_db()
 
 app.include_router(auth_router)
 
-@app.get("/")
-def home():
-    return {"mensagem": "API de correção ortográfica/gramatical do MapaZZZ ativa!"}
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
 @app.post("/corrigir-frase")
 def corrigir_texto(data: dict, Authorize: AuthJWT = Depends()):
     Authorize.jwt_required()
     frase = data.get("frase", "")
-    return {"frase_corrigida": corrigir_frase(frase)}
-
+    return {"frase corrigida": corrigir_frase(frase)}
+    
 if __name__ == "__main__":
     import uvicorn
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
+
